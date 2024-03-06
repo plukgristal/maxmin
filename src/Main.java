d629fc18691948a11ce7b7654bbb9173be4ea9a4
plugristal <silaby84@gmail.com>
2024-03-06 18:13:33 +0100

    Max and min

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+    System.out.println("Enter two numbers");
+    Scanner scanner = new Scanner(System.in);
+
+    String snum1 = scanner.next();
+    String snum2 = scanner.next();
+
+    int num1 = Integer.parseInt(snum1);
+    int num2 = Integer.parseInt(snum2);
+
+    if(num1 > num2){
+        System.out.println("The largest number is " + num1);
+    }else if(num1 < num2){
+        System.out.println("The largest number is " + num2);
+    }else{
+        System.out.println("The numbers are equal");
+    }
     }
 }
